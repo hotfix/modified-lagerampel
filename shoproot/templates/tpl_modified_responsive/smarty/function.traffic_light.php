9a415b5d1a04b6783148ae4c798f9a7d95ade5e8
demo-modules <120286459+demo-modules@users.noreply.github.com>
2023-06-15 21:55:01 +0200

    fix clause

--- shoproot/templates/tpl_modified_responsive/smarty/function.traffic_light.php
+++ shoproot/templates/tpl_modified_responsive/smarty/function.traffic_light.php
@@ -35,12 +35,10 @@ function smarty_function_traffic_light($params, &$smarty) {
       if ($stock <= MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL) {
           $stock_info = '<span class="traff-light"><strong>'.MODULE_TRAFFIC_LIGHTS_STOCK.':</strong> <span class="tl zero-tl"></span><span class="tl zero-tl"></span><span class="tl red-tl"></span><span class="nr-tooltip red-tl" aria-label="'.MODULE_TRAFFIC_LIGHTS_STOCK.'">'.MODULE_TRAFFIC_LIGHTS_QTY_RED.' | '.$stock.'</span></span>';
           $html .= $stock_info;
-      } 
-      elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL && $stock < MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
+      } elseif ($stock > MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL && $stock < MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
           $stock_info = '<span class="traff-light"><strong>'.MODULE_TRAFFIC_LIGHTS_STOCK.':</strong> <span class="tl zero-tl"></span><span class="tl yell-tl"></span><span class="tl zero-tl"></span><span class="nr-tooltip yell-tl yell-txt" aria-label="'.MODULE_TRAFFIC_LIGHTS_STOCK.'">'.MODULE_TRAFFIC_LIGHTS_QTY_YELL.' | '.$stock.'</span></span>';
           $html .= $stock_info;
-      }
-      elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
+      } elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
           $stock_info = '<span class="traff-light"><strong>'.MODULE_TRAFFIC_LIGHTS_STOCK.':</strong> <span class="tl green-tl"></span><span class="tl zero-tl"></span><span class="tl zero-tl"></span><span class="nr-tooltip green-tl" aria-label="'.MODULE_TRAFFIC_LIGHTS_STOCK.'">'.MODULE_TRAFFIC_LIGHTS_QTY_GREEN.' | '.$stock.'</span></span>';
           $html .= $stock_info;
       }
@@ -77,17 +75,15 @@ function smarty_function_traffic_light($params, &$smarty) {
       $stock_title = '<span title="#stock_title#" aria-label="#stock_title#">#stock_info#</span>';
       $stock_title = $brackets ? str_replace('#stock_info#',' (#stock_info#)',$stock_title) : $stock_title;
       
-      if ($stock < MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL) {
+      if ($stock <= MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL) {
           $stock_txt = MODULE_TRAFFIC_LIGHTS_QTY_RED. ($show_qty ? ' '. $stock : '');
           $stock_info = str_replace(array('tl green-tl','tl yell-tl','stock-txt','#stock_title#'),array('tl zero-tl','tl zero-tl','red-txt',$stock_txt),$stock_info);
           $html .= str_replace(array('#stock_title#','#stock_info#'),array($stock_txt,$stock_info),$stock_title);
-      } 
-      elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL && $stock < MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
+      } elseif ($stock > MODULE_TRAFFIC_LIGHTS_STOCK_RED_YELL && $stock < MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
           $stock_txt = MODULE_TRAFFIC_LIGHTS_QTY_YELL . ($show_qty ? ' '. $stock : '');
           $stock_info = str_replace(array('tl green-tl','stock-txt','tl red-tl','#stock_title#'),array('tl zero-tl','yell-txt','tl zero-tl',$stock_txt),$stock_info);
           $html .= str_replace(array('#stock_title#','#stock_info#'),array($stock_txt,$stock_info),$stock_title); 
-      }
-      elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
+      } elseif ($stock >= MODULE_TRAFFIC_LIGHTS_STOCK_GREEN) {
           $stock_txt = MODULE_TRAFFIC_LIGHTS_QTY_GREEN. ($show_qty ? ' '. $stock : '');
           $stock_info = str_replace(array('stock-txt','tl yell-tl','tl red-tl','#stock_title#'),array('green-txt','tl zero-tl','tl zero-tl',$stock_txt),$stock_info);
           $html .= str_replace(array('#stock_title#','#stock_info#'),array($stock_txt,$stock_info),$stock_title); 
